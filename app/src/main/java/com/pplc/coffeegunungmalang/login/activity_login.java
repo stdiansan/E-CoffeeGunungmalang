567ce05ed416b6650334289882256d6f91d1bec5
stdiansan <stefdian19d@outlook.com>
2019-09-25 13:54:52 +0700

    penambahan icon drawable

---
+++ app/src/main/java/com/pplc/coffeegunungmalang/login/activity_login.java
@@ -0,0 +1,16 @@
+package com.pplc.coffeegunungmalang.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pplc.coffeegunungmalang.R;
+
+public class activity_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
