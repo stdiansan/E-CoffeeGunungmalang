a55843118523bfd5575652cb4a437a40b8597bfd
stdiansan <stefdian19d@outlook.com>
2019-09-25 22:46:27 +0700

    menambahkan fragment

--- app/src/main/java/com/pplc/coffeegunungmalang/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/pplc/coffeegunungmalang/ui/profile/ProfileFragment.java
@@ -1,4 +1,4 @@
-package com.pplc.coffeegunungmalang.ui.dashboard;
+package com.pplc.coffeegunungmalang.ui.profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,15 +14,15 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.pplc.coffeegunungmalang.R;
 
-public class DashboardFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private ProfileViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+                ViewModelProviders.of(this).get(ProfileViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
         dashboardViewModel.getText().observe(this, new Observer<String>() {
             @Override
--- app/src/main/java/com/pplc/coffeegunungmalang/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/pplc/coffeegunungmalang/ui/profile/ProfileViewModel.java
@@ -1,14 +1,14 @@
-package com.pplc.coffeegunungmalang.ui.dashboard;
+package com.pplc.coffeegunungmalang.ui.profile;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class ProfileViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public ProfileViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
