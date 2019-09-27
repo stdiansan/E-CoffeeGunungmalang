e13100b566d6873a7d829b385e13d45913ff69a1
stdiansan <stefdian19d@outlook.com>
2019-09-27 17:23:47 +0700

    menambahkan fragment

--- app/src/main/java/com/pplc/coffeegunungmalang/activity_splashscreen.java
+++ app/src/main/java/com/pplc/coffeegunungmalang/activity_splashscreen.java
@@ -1,14 +1,27 @@
 package com.pplc.coffeegunungmalang;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+
+import com.pplc.coffeegunungmalang.login.activity_login;
 
 public class activity_splashscreen extends AppCompatActivity {
-
+    private int waktu_loading=1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //setelah loading maka akan langsung berpindah ke home activity
+                startActivity(new Intent(activity_splashscreen.this, activity_login.class));
+                finish();
+
+            }
+        },waktu_loading);
+
     }
 }
--- app/src/main/java/com/pplc/coffeegunungmalang/login/activity_login.java
+++ app/src/main/java/com/pplc/coffeegunungmalang/login/activity_login.java
@@ -2,9 +2,15 @@ package com.pplc.coffeegunungmalang.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.pplc.coffeegunungmalang.MainActivity;
 import com.pplc.coffeegunungmalang.R;
+import com.pplc.coffeegunungmalang.register.activity_register;
 
 public class activity_login extends AppCompatActivity {
 
@@ -12,5 +18,29 @@ public class activity_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnLogin, btnDaftar;
+        EditText etEmail, etPassword;
+
+        btnLogin = findViewById(R.id.login_masuk);
+        btnDaftar = findViewById(R.id.login_register);
+        etEmail = findViewById(R.id.login_email);
+        etPassword = findViewById(R.id.login_password);
+
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(activity_login.this,activity_register.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(activity_login.this, MainActivity.class));
+            }
+        });
+
+
     }
 }
